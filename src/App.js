9db2034016bb23cad8f659b77831d8072ce72947
mdakeel <aakiltayyab@gmail.com>
2024-06-24 10:03:45 +0530

    dukaan first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { BrowserRouter } from 'react-router-dom';
+
 import './App.css';
+import Header from './components/Header';
 
 
 function App() {
   return (
-      <BrowserRouter>
-        <div>hello world</div>
-      </BrowserRouter>
+        <div className='w-screen h-auto flex flex-col'>
+            <Header />
+        </div>
     );
 }
 
