3f008bc95b3a41c941b617ba1fbc5b4d620d5380
mdakeel <aakiltayyab@gmail.com>
2024-07-04 13:08:04 +0530

    new commit

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,7 +1,7 @@
 import I1 from "../assets/i1.png";
 import F1 from "../assets/f1.png";
 import C3 from "../assets/c3.png";
-import Fi1 from "../assets/fi1.png";
+import c1 from "../assets/c1.png";
 
 export const heroData = [
   {
@@ -30,7 +30,7 @@ export const heroData = [
     name: "Fish Kebab",
     decription: "Mixed Fish Kebab",
     price: "5.25",
-    imageSrc: Fi1,
+    imageSrc: c1,
   },
 ];
 
